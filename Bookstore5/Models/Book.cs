5060a1a453bdd9ef90f69a4889a64928c506f6db
mintyfreshfish <lukybecca@gmail.com>
2021-02-17 21:07:54 -0800

    first commit

---
+++ Bookstore5/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore5.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string Class { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Bookstore5/Models/BookstoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore5.Models
+{
+    public class BookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext _context;
+
+        //Constructor
+        public BookstoreRepository(BookstoreContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
